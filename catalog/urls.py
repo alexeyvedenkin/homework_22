5128cb30d0fbf34e752d04dc612f3d7eddd7b92f
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-05-15 17:28:40 +0500

    добавлен контроллер contact_answer

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,4 +8,5 @@ app_name = 'catalog'
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
+    path('contact_answer/', views.contact_answer, name='contact_answer'),
 ]
