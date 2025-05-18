03f1f9da44d437534742c635d77c49173c0d1fab
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-05-18 14:34:46 +0500

    обработка линтерами

--- catalog/views.py
+++ catalog/views.py
@@ -1,21 +1,22 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
+    """ Выполняет переход к странице catalog/home.html """
     return render(request, 'catalog/home.html')
 
 
 def contacts(request):
+    """ Выполняет переход к странице catalog/contacts.html """
     return render(request, 'catalog/contacts.html')
 
 
 def contact_answer(request):
+    """ Выполняет запрос сообщения от пользователя.
+        Возвращает уведомление о получении данных """
     if request.method == 'POST':
-        # Получение данных из формы
         name = request.POST.get('name')
         message = request.POST.get('message')
-        # Обработка данных (например, сохранение в БД, отправка email и т. д.)
-        # Здесь мы просто возвращаем простой ответ
         return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
     return render(request, 'catalog/contacts.html')
