17a62fc026c63219b6e293852302ecab130acf17
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-05-26 22:47:26 +0500

    реализована кастомная команда добавления данных из фикстур

---
+++ catalog/management/commands/add_elements.py
@@ -0,0 +1,16 @@
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+
+from catalog.models import Category, Product
+
+
+class Command(BaseCommand):
+    help = 'Загружает данные из фикстур в базу данных'
+
+    def handle(self, *args, **kwargs):
+
+        Product.objects.all().delete()
+        Category.objects.all().delete()
+
+        call_command('loaddata', 'catalog')
+        self.stdout.write(self.style.SUCCESS(f'Successfully loaded data.'))
