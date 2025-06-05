81ea4ae7eda6c94337aa6ab95dd8e49e5650af52
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-06-06 00:30:42 +0500

    настройка админ-панели

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Article
+
+
+@admin.register(Article)
+class ArticletAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "content", "active_is", "views_counter")
+    list_filter = ("active_is",)
+    search_fields = ("title", "active_is")
