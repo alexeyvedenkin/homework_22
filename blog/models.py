7d6c06edb1fa2cfca478be7e73f0e20d3546e613
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-06-05 23:59:23 +0500

    создана модель Article в приложении blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(
+        max_length=100, verbose_name="Статья",
+    )
+    content = models.TextField(
+        verbose_name="Содержание",
+    )
+    preview = models.ImageField(
+        upload_to="article/images",
+        verbose_name="Изображение",
+        blank=True,
+        null=True,
+    )
+    created_at = models.DateField(
+        verbose_name="Дата создания",
+        blank=True,
+        null=True,
+    )
+    active_is = models.BooleanField(
+        verbose_name="Признак публикации",
+        blank=True,
+        null=True,
+    )
+    views_counter = models.PositiveIntegerField(
+        verbose_name="Количество просмотров",
+        default=0
+    )
+
+    class Meta:
+        verbose_name = "Статья"
+        verbose_name_plural = "Статьи"
+        ordering = ["views_counter"]
