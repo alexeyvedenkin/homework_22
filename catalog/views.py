9dd37c5f31aefeb86388598a64eee83b9ced24b4
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-06-05 21:36:51 +0500

    настроены контроллеры для приложения catalog с использованием CBV

--- catalog/views.py
+++ catalog/views.py
@@ -1,61 +1,39 @@
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, TemplateView, UpdateView
 
-from catalog.models import Product, Category
-
-
-def base(request):
-    """ Выполняет переход к странице catalog/base.html """
-    return render(request, 'catalog/base.html')
+from catalog.models import Product
 
 
-def contacts(request):
+class ProductListView(ListView):
+    model = Product
+
+
+class ProductDetailView(DetailView):
+    model = Product
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ('name', 'description', 'price', 'create_date', 'product_image')
+    success_url = reverse_lazy('catalog:product_list')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ('name', 'description', 'price', 'create_date', 'product_image')
+    success_url = reverse_lazy('catalog:product_list')
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('catalog:product_list')
+
+
+class ContactsTemplateView(TemplateView):
     """ Выполняет переход к странице catalog/contacts.html """
-    return render(request, 'catalog/contacts.html')
+    template_name = 'catalog/contacts.html'
 
 
-def contact_answer(request):
-    """ Выполняет запрос сообщения от пользователя.
-        Возвращает уведомление о получении данных """
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        message = request.POST.get('message')
-        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
-    return render(request, 'catalog/contacts.html')
-
-
-def product_detail(request,pk):
-    """ Возвращает детальную информацию о товаре """
-    product = get_object_or_404(Product, pk=pk)
-    context= {'product': product}
-    return render(request, 'catalog/product_detail.html', context)
-
-
-def test_base(request):
-    """ Выполняет переход к странице catalog/test_base.html """
-    return render(request, 'catalog/test_base.html')
-
-
-def index(request):
-    """ Выполняет переход к странице catalog/test_base.html """
-    return render(request, 'catalog/index.html')
-
-
-def orders(request):
-    """ Выполняет переход к странице catalog/test_base.html """
-    return render(request, 'catalog/orders.html')
-
-
-def catalog(request):
-    """ Выполняет переход к странице catalog/test_base.html """
-    return render(request, 'catalog/catalog.html')
-
-
-def home(request):
-    products = Product.objects.all()  # Извлечение всех продуктов
-    return render(request, 'catalog/home.html', {'products': products})
-
-
-def catalog_view(request):
-    products = Product.objects.all()
-    return render(request, 'home.html', {'products': products})
+class HomeTemplateView(TemplateView):
+    """ Выполняет переход к странице catalog/contacts.html """
+    template_name = 'catalog/home.html'
