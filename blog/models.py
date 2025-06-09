cb2dbde29884ead80fa65dfcd64313c70210817a
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-06-09 18:18:18 +0500

    fixed blog/models.py

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class Article(models.Model):
     title = models.CharField(
-        max_length=100, verbose_name="Статья",
+        max_length=100,
+        verbose_name="Статья",
     )
     content = models.TextField(
         verbose_name="Содержание",
@@ -14,22 +15,20 @@ class Article(models.Model):
         blank=True,
         null=True,
     )
-    created_at = models.DateField(
-        verbose_name="Дата создания",
-        blank=True,
-        null=True,
+    create_at = models.DateTimeField(
+        auto_now_add=True,
     )
     active_is = models.BooleanField(
         verbose_name="Признак публикации",
-        blank=True,
-        null=True,
     )
     views_counter = models.PositiveIntegerField(
-        verbose_name="Количество просмотров",
-        default=0
+        verbose_name="Количество просмотров", default=0
     )
 
     class Meta:
         verbose_name = "Статья"
         verbose_name_plural = "Статьи"
         ordering = ["views_counter"]
+
+    def __str__(self):
+        return self.title
