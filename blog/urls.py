6a7a188430e3de6fe99145a8fac710aa391e08e9
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-06-09 18:21:17 +0500

    донастроена маршрутизация

--- blog/urls.py
+++ blog/urls.py
@@ -6,24 +6,17 @@ from blog.views import (
     ArticleCreateView,
     ArticleUpdateView,
     ArticleDeleteView,
-    ContactsTemplateView,
     HomeTemplateView,
 )
 
 app_name = BlogConfig.name
 
 urlpatterns = [
-    path("product_list/", ArticleListView.as_view(), name="product_list"),
-    path(
-        "product_detail/<int:pk>/", ArticleDetailView.as_view(), name="product_detail"
-    ),
-    path("product_create/", ArticleCreateView.as_view(), name="product_create"),
-    path(
-        "product/<int:pk>/update/", ArticleUpdateView.as_view(), name="product_update"
-    ),
-    path(
-        "product/<int:pk>/delete/", ArticleDeleteView.as_view(), name="product_delete"
-    ),
+    path("blog/article_list/", ArticleListView.as_view(), name="article_list"),
+    path("blog/article_create/", ArticleCreateView.as_view(), name="article_create"),
+    path("blog/article_detail/<int:pk>/", ArticleDetailView.as_view(), name="article_detail"),
+    path("blog/article/update/<int:pk>/", ArticleUpdateView.as_view(), name="article_form"),
+    path("blog/article/<int:pk>/delete/", ArticleDeleteView.as_view(), name="article_delete"),
     path("", HomeTemplateView.as_view(), name="home"),
-    path("contacts/", ContactsTemplateView.as_view(), name="contacts"),
+
 ]
