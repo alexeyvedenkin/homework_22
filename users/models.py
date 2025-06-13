854458350a023a6125a18b1291fc521f03c85fe1
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-06-13 11:44:01 +0500

    fixed users/models.py

--- users/models.py
+++ users/models.py
@@ -3,13 +3,17 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
-    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    email = models.EmailField(unique=True, verbose_name='Email')
+    phone_number = models.CharField(max_length=15, verbose_name='Телефон', blank=True, null=True)
+    avatar = models.ImageField(upload_to='users/avatars/', verbose_name='Аватар', blank=True, null=True)
     country = models.CharField(max_length=25, blank=True, null=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username',]
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
 
     def __str__(self):
         return self.email
