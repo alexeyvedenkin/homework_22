dc3d6b4c25bf09a9e7706dc07711b85fdc9f4bb8
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-06-13 16:14:52 +0500

    настроена маршрутизация для работы с моделью пользователя

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from users.apps import UsersConfig
-from .views import RegisterView
+from .views import RegisterView, email_verification
 
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('login/', LoginView.as_view(template_name='users/login.html', success_url='/catalog/product_list/'), name='login'),
     path('logout/', LogoutView.as_view(next_page='catalog:product_list'), name='logout'),
+    path('email-confirm/<str:token>/', email_verification, name='email-confirm')
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,12 +1,43 @@
-from django.urls import reverse_lazy
+import os
+import secrets
+
+from django.core.mail import send_mail
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse_lazy, reverse
 from django.views.generic.edit import CreateView
+from pyexpat.errors import messages
+
 from .forms import CustomUserCreationForm
+from .models import CustomUser
+
+from config.settings import EMAIL_HOST_USER
 
 
 class RegisterView(CreateView):
+    model = CustomUser
     template_name = 'users/register.html'
     form_class = CustomUserCreationForm
-    success_url = reverse_lazy('catalog:product_list')
-    
+    success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        user = form.save()
+        user.is_active = False
+        token = secrets.token_hex(16)
+        user.token = token
+        user.save()
+        host = self.request.get_host()
+        url = f'http://{host}/users/email-confirm/{token}/'
+        send_mail(
+            subject="Подтверждение почты",
+            message=f"Привет, перейди по ссылке для подтверждения регистрации {url}",
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user.email]
+        )
+        return super().form_valid(form)
 
 
+def email_verification(request, token):
+    user = get_object_or_404(CustomUser, token=token)
+    user.is_active = True
+    user.save()
+    return redirect(reverse('users:login'))
