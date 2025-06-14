a04ba549207508718962c1326e6f3f30fbea37e3
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-06-14 15:01:00 +0500

    настроен класс StyleFormMixin для стилизации шаблонов форм

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,21 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.forms import BooleanField
 
 from .models import CustomUser
 
 
-class CustomUserCreationForm(UserCreationForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+
+
+class CustomUserCreationForm(StyleFormMixin, UserCreationForm):
     phone_number = forms.CharField(max_length=15, required=False, help_text='Необязательное поле. Введите ваш номер телефона')
     username = forms.CharField(max_length=50, required=True)
 
@@ -17,3 +28,9 @@ class CustomUserCreationForm(UserCreationForm):
         if phone_number and not phone_number.isdigit():
             raise forms.ValidationError('Номер телефона должен содержать только цифры')
         return phone_number
+
+
+class CustomAuthenticationForm(StyleFormMixin, AuthenticationForm):
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'password')
