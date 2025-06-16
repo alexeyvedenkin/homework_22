3fa506ceb676f29bdf1a23661e92dc924476ae70
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-06-16 21:04:48 +0500

    обновлены шаблоны

--- catalog/urls.py
+++ catalog/urls.py
@@ -15,16 +15,10 @@ app_name = CatalogConfig.name
 
 urlpatterns = [
     path("product_list/", ProductListView.as_view(), name="product_list"),
-    path(
-        "product_detail/<int:pk>/", ProductDetailView.as_view(), name="product_detail"
-    ),
+    path("product_detail/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
     path("product_create/", ProductCreateView.as_view(), name="product_create"),
-    path(
-        "product/<int:pk>/update/", ProductUpdateView.as_view(), name="product_update"
-    ),
-    path(
-        "product/<int:pk>/delete/", ProductDeleteView.as_view(), name="product_delete"
-    ),
+    path("product/<int:pk>/update/", ProductUpdateView.as_view(), name="product_update"),
+    path("product/<int:pk>/delete/", ProductDeleteView.as_view(), name="product_delete"),
     path("", HomeTemplateView.as_view(), name="home"),
     path("contacts/", ContactsTemplateView.as_view(), name="contacts"),
     path("non_published_products/", NonPublishedProductListView.as_view(), name="non_published_products"),
