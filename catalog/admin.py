86c405e9122ec295309acf48c2e13cdb14dfadeb
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-06-16 16:25:57 +0500

    настроена отображение товаров в админ-панели

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,8 +5,8 @@ from catalog.models import Category, Product
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "price", "category")
-    list_filter = ("category",)
+    list_display = ("id", "name", "price", "category", "owner", "is_published")
+    list_filter = ("category", "is_published", "owner")
     search_fields = ("name", "description")
 
 
