3bf58dbadcea7722387e0ab14d78c7d6b2d54e6f
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-06-17 21:28:33 +0500

    настроены права доступа в контроллерах приложения blog

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 import os
 
+from django.contrib.auth.decorators import login_required, permission_required
+from django.shortcuts import get_object_or_404, redirect
 from dotenv import load_dotenv
 from django.core.mail import send_mail
 from django.urls import reverse_lazy, reverse
@@ -74,6 +76,24 @@ class ArticleUpdateView(UpdateView):
 class ArticleDeleteView(DeleteView):
     model = Article
     success_url = reverse_lazy("blog:article_list")
+
+
+@login_required
+@permission_required('blog.can_unpublish_article', raise_exception=True)
+def publish_article(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+    article.is_published = True
+    article.save()
+    return redirect('blog:non_published_article')
+
+
+@login_required
+@permission_required('blog.can_unpublish_product', raise_exception=True)
+def unpublish_article(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+    article.is_published = False
+    article.save()
+    return redirect('blog:non_published_article')
 
 
 class ContactsTemplateView(TemplateView):
